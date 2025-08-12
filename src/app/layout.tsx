0b39b34ac3511af372622ec8529624e4524fa319
Shreeshkumar P <shreeshbhat0509@gmail.com>
2025-08-12 09:11:34 +0000

    use back ground in black color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn("min-h-screen bg-background font-body antialiased")}>
+      <body className={cn("min-h-screen bg-background font-body antialiased dark")}>
         <AuthProvider>
           <div className="relative flex min-h-dvh flex-col">
             <Header />
