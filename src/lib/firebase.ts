bcf7efdc8b7c0f5fd4211f62830f1c6a684d25fa
Shreeshkumar P <shreeshbhat0509@gmail.com>
2025-09-10 09:44:11 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  "projectId": "moodlight-lah48",
-  "appId": "1:39982483307:web:ee9bb2e583603b5581ae2e",
-  "storageBucket": "moodlight-lah48.firebasestorage.app",
-  "apiKey": "AIzaSyAN5ZXE5kFJAbljbqR5rq-_xGbfQ22SskM",
-  "authDomain": "moodlight-lah48.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "39982483307",
-  "databaseURL": "https://moodlight-lah48.firebaseio.com"
+  projectId: "moodlight-lah48",
+  appId: "1:39982483307:web:ee9bb2e583603b5581ae2e",
+  storageBucket: "moodlight-lah48.firebasestorage.app",
+  apiKey: "AIzaSyAN5ZXE5kFJAbljbqR5rq-_xGbfQ22SskM",
+  authDomain: "moodlight-lah48.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "39982483307",
+  databaseURL: "https://moodlight-lah48.firebaseio.com"
 };
 
 
