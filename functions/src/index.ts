11d494a252dc1b6ad214ae1baabb9bbc83b7059f
Shreeshkumar P <shreeshbhat0509@gmail.com>
2025-09-17 14:12:04 +0000

    are u fine now

--- functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-// functions/src/index.ts
-
-// This file is no longer in use. The alerting logic has been moved to 
-// the `mood-truthfulness-flow.ts` file to bypass the need for
-// Cloud Function deployment and the associated billing account requirement.
-// You can safely ignore or delete this file.
-
