794de56126099e32a54fa433b581953fff64d822
Shreeshkumar P <shreeshbhat0509@gmail.com>
2025-11-14 17:52:37 +0000

    Try fixing this error: `Runtime Error: Error: PERMISSION_DENIED: Permiss

--- src/firebase.ts
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAN5ZXE5kFJAbljbqR5rq-_xGbfQ22SskM",
-    authDomain: "moodlight-lah48.firebaseapp.com",
-    databaseURL: "https://moodlight-lah48-default-rtdb.firebaseio.com",
-    projectId: "moodlight-lah48",
-    storageBucket: "moodlight-lah48.firebasestorage.app",
-    messagingSenderId: "39982483307",
-    appId: "1:39982483307:web:ee9bb2e583603b5581ae2e"
-  };
