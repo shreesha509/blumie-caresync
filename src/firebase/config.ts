cea2c0eb8eb7ec42b9bf0b7d8c92b9d24809888d
Shreeshkumar P <shreeshbhat0509@gmail.com>
2025-11-14 17:47:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "moodlight-lah48",
+  "appId": "1:39982483307:web:ee9bb2e583603b5581ae2e",
+  "apiKey": "AIzaSyAN5ZXE5kFJAbljbqR5rq-_xGbfQ22SskM",
+  "authDomain": "moodlight-lah48.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "39982483307"
+};
